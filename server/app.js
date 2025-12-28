c0a2252d446591f08b3cacf7223aaabf8f391fb8
Mohit Pramanik <mohitpramanik548@gmail.com>
2025-12-28 20:18:40 +0530

    testing deployment changes

--- server/app.js
+++ server/app.js
@@ -22,6 +22,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", (req, res) => {
+    return res.send("Hello world");
+})
+
 app.use("/api/user", userRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/comment", commentRouter);
